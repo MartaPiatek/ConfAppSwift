21b2001755f9cf98135272a6b664ca101ff47ea6
Marta Piątek <martusia.piatek@gmail.com>
2018-06-01 20:13:05 +0200

    Initial Commit

---
+++ ConfApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConfApp
+//
+//  Created by Marta Piątek on 01.06.2018.
+//  Copyright © 2018 Marta Piątek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
