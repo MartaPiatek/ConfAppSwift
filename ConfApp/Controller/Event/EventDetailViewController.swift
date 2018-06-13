168da87d1da259591ce504da1a86de93afbc9044
Marta Piątek <martusia.piatek@gmail.com>
2018-06-13 16:53:46 +0200

    Refaktoring

--- ConfApp/Controller/Event/EventDetailViewController.swift
+++ ConfApp/Controller/Event/EventDetailViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class EventDetailViewController: UIViewController {
 
@@ -24,6 +25,9 @@ class EventDetailViewController: UIViewController {
     var localizationValue: String = ""
     var abstractValue: String = ""
     
+    let ref = Database.database().reference(withPath: "notes")
+    
+    var notes = [Note]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,6 +45,37 @@ class EventDetailViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func addNoteButton(_ sender: AnyObject) {
+        let alert = UIAlertController(title: "Note Item",
+                                      message: "Add an Item",
+                                      preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
+            guard let textField = alert.textFields?.first,
+                let text = textField.text else {return}
+            
+            
+            let userId = Auth.auth().currentUser!.uid
+            let noteItem = Note(user: userId, eventTitle: self.titleValue, content: text)
+           
+            
+            let userItemRef = self.ref.child(userId)
+            
+            userItemRef.setValue(noteItem.toAnyObject())
+            //   self.items.append(groceryItem)
+            // self.tableView.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .cancel)
+        
+        alert.addTextField()
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
 
     /*
     // MARK: - Navigation
