c99faf0bb71cc3e66cff00942c68f2fa3291a449
Marta Piątek <martusia.piatek@gmail.com>
2018-06-28 19:37:41 +0200

    Dodanie widoku informacji o konferencji

---
+++ ConfApp/Controller/AboutViewController.swift
@@ -0,0 +1,47 @@
+//
+//  AboutViewController.swift
+//  ConfApp
+//
+//  Created by Marta Piątek on 28.06.2018.
+//  Copyright © 2018 Marta Piątek. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        assignbackground()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    public func assignbackground(){
+        let background = UIImage(named: "background2")
+        
+        var imageView : UIImageView!
+        imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode =  UIViewContentMode.scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = background
+        imageView.center = view.center
+        imageView.alpha = 0.55
+        view.addSubview(imageView)
+        self.view.sendSubview(toBack: imageView)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
